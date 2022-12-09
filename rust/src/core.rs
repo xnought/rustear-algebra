c7ef522526baf5b705f4287978e80d0a8e9c1643
xnought <bertuccd@oregonstate.edu>
2022-12-08 22:11:39 -0800

    feat: start tensor

--- rust/src/core.rs
+++ rust/src/core.rs
@@ -1,3 +1,20 @@
-pub fn tensor() {
-    println!("Hello, tensor!");
+pub struct Tensor<'a> {
+    pub data: &'a mut Vec<f32>,
+    pub shape: Vec<usize>,
+    pub stride: Vec<usize>,
+    pub offset: usize,
+}
+
+impl Tensor<'_> {
+    pub fn print(&self) {
+        for i in 0..self.shape[0] {
+            for j in 0..self.shape[1] {
+                print!(
+                    "{} ",
+                    self.data[self.offset + i * self.stride[0] + j * self.stride[1]]
+                );
+            }
+            println!();
+        }
+    }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,33 @@
 mod core;
 
 fn main() {
-    core::tensor();
+    let mut data: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+    let a = core::Tensor {
+        data: &mut data,
+        shape: vec![3, 3],
+        stride: vec![3, 1],
+        offset: 0,
+    };
+    a.print();
+    println!();
+
+    // get column
+    let b = core::Tensor {
+        data: &mut data,
+        shape: vec![3, 1],
+        stride: vec![3, 0],
+        offset: 0,
+    };
+    b.print();
+    println!();
+
+    // get row
+    let c = core::Tensor {
+        data: &mut data,
+        shape: vec![1, 3],
+        stride: vec![0, 1],
+        offset: 0,
+    };
+    c.print();
+    println!();
 }
